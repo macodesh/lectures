12701bca51eef02027a5854d6191689c08a177d0
Marlon Couto <devpatola@proton.me>
2023-08-30 16:05:14 -0300

    Atualiza httpStatus.js

--- http-server/httpStatus.js
+++ http-server/httpStatus.js
@@ -1,13 +1,23 @@
 // Mensagens de sucesso
-export const OK = 200;
-export const CREATED = 201;
-export const NO_CONTENT = 204;
+const OK = 200;
+const CREATED = 201;
+const NO_CONTENT = 204;
 
 // Mensagens de erro
-export const BAD_REQUEST = 400;
-export const UNAUTHORIZED = 401;
-export const NOT_FOUND = 404;
-export const CONFLICT = 409;
+const BAD_REQUEST = 400;
+const UNAUTHORIZED = 401;
+const NOT_FOUND = 404;
+const CONFLICT = 409;
 
 // Erro interno do servidor
-export const INTERNAL_ERROR = 500;
+const INTERNAL_ERROR = 500;
+
+module.export = {
+  OK,
+  CREATED,
+  NO_CONTENT,
+  BAD_REQUEST,
+  UNAUTHORIZED,
+  NOT_FOUND,
+  CONFLICT,
+};
